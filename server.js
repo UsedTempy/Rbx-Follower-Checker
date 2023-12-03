40510c75ab63ba1d1a887fce0f928c716e81a8d1
UsedTemplate <98837995+UsedTemplate@users.noreply.github.com>
2023-12-03 14:45:35 +0100

    Added error catching and increased the sortOrder

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express')
 const app = express()
 
+const PORT = 3000;
+
 app.use(express.json())
 
-app.get("/IsFollowing/:Id", (req, res) => {
+app.get("/IsFollowing/:Id/:hasToFollowId", (req, res) => {
   const id = req.params.Id
-  const isFollowing = false;
+  const hasToFollowId = req.params.hasToFollowId
+  var isFollowing = false;
 
-  fetch(`https://friends.roblox.com/v1/users/${id}/followings?limit=10&sortOrder=Asc`)
+  fetch(`https://friends.roblox.com/v1/users/${id}/followings?limit=101&sortOrder=Desc`)
   .then(res => res.json())
   .then(data => {
-    res.status(200).json(data)
+    for (let i = 0; i < data.data.length; i++) {
+        if (data.data[i].id == hasToFollowId) {
+            isFollowing = true
+            break
+        }   
+    }
+
+    res.status(200).json({
+      response: isFollowing
+    })
+  })
+  .catch(error => {
+    res.status(404).json({
+        response: false
+    })
   })
 })
 
-app.listen(3000, () => console.log(`Server running at https://localhost:${3000}`))
+app.listen(PORT, () => console.log(`Server running at https://localhost:${PORT}`))
