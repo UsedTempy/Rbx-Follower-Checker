dad962908b3f49d6c1f912568d993952f5f34758
Tempy <UsedTemplate@gmail.com>
2023-12-06 14:01:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,28 +5,26 @@ const PORT = 3000;
 
 app.use(express.json())
 
-app.get("/IsFollowing/:UserId/:InputId", (req, res) => {
-  const id = req.params.UserId
-  const InputId = req.params.InputId
-  var isFollowing = false;
+app.get("/IsFollowing/:id/:id_check", (req, res) => {
+  const id = req.params.id
+  const id_check = req.params.id_check
 
   fetch(`https://friends.roblox.com/v1/users/${id}/followings?limit=100&sortOrder=Desc`)
   .then(res => res.json())
   .then(data => {
-    for (let i = 0; i < data.data.length; i++) {
-        if (data.data[i].id == InputId) {
-            isFollowing = true
-            break
-        }
-    }
+    const isFollowing = data.data.some(item => item.id == id_check);
 
     res.status(200).json({
       response: isFollowing
-    })
+    });
   })
   .catch(error => {
-    res.status(404).json({response: false})
-  })
+    console.error("Error:", error);
+    // Handle the error appropriately
+    res.status(500).json({
+      error: "Internal Server Error"
+    });
+  });
 })
 
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
